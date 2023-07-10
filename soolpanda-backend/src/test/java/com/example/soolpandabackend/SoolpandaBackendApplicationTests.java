60612dc02beca08484896e5c3c6763e77dd2a689
mrcsbin <81237987+mrcsbin@users.noreply.github.com>
2023-07-10 12:15:29 +0900

    Add files via upload

---
+++ soolpanda-backend/src/test/java/com/example/soolpandabackend/SoolpandaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soolpandabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoolpandaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
